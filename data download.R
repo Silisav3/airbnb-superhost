f4f20f42a89cd26cd83a6694a55248306281aeaa
Silisav3 <82175442+Silisav3@users.noreply.github.com>
2021-09-08 23:48:19 +0200

    Add files via upload

---
+++ data download.R
@@ -0,0 +1,13 @@
+library(googledrive)
+library(readr)
+
+data_id <- "https://drive.google.com/file/d/17FFZX3t8ibGp1X2AaFHMwBg0kMhHGb-g/view?usp=sharing"
+out_file <- "Hong_Kong.csv" 
+
+drive_download(
+    as_id(data_id), 
+    path = out_file,
+    overwrite = TRUE)
+
+    
+    
