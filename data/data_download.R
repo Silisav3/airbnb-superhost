4dcb694e8875b01a90e6aa43ec51343047e0dfb5
Silisav3 <V.Syrpas@tilburguniversity.edu>
2021-09-19 14:35:04 +0200

    Google drive - Clean data- fix folders

--- data/data_download.R
+++ data/data_download.R
@@ -2,12 +2,15 @@
 library(googledrive)
 library(readr)
 
+# --- Download data --- #
 data_id <- "https://drive.google.com/file/d/17FFZX3t8ibGp1X2AaFHMwBg0kMhHGb-g/view?usp=sharing"
-out_file <- "Hong_Kong.csv" 
+out_file <- "Amsterdam.csv" 
 
 drive_download(
     as_id(data_id), 
     path = out_file,
     overwrite = TRUE)
 
-    
+Amsterdam_unclean <- read.csv("Amsterdam.csv")
+
+
