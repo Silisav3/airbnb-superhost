6ac8e884e17106d043f9bd39cd23018e3104b431
Silisav3 <V.Syrpas@tilburguniversity.edu>
2021-10-15 15:34:23 +0200

    create folders

--- src/data-prep/data_download.R
+++ src/data-prep/data_download.R
@@ -5,6 +5,13 @@ pacman::p_load(googledrive,readr,MatchIt, optmatch, dplyr, gapminder, fixest, fo
 
 search()
 
+# Create folders 
+dir.create('./data')
+dir.create('./gen/input')
+dir.create('./gen/temp')
+dir.create('./gen/output')
+
+
 # --- Download data --- #
 data_id <- "https://drive.google.com/file/d/17FFZX3t8ibGp1X2AaFHMwBg0kMhHGb-g/view?usp=sharing"
 out_file <- "data/Amsterdam.csv" 
@@ -13,10 +20,4 @@ drive_download(
     as_id(data_id), 
     path = out_file,
     overwrite = TRUE)
-
-
-# Create folders 
-dir.create('./data')
-dir.create('./gen/input')
-dir.create('./gen/temp')
 
